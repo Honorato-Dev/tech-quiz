1b0f396584bcc27c0a606c7dd15078380bdb24d0
vituhonorato <vituhonorato.89@gmail.com>
2023-11-18 18:49:42 -0300

    add models/User

---
+++ models/User.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
