5ede08cee5f9a260e67b5d7e384571a107db85b6
vituhonorato <vituhonorato.89@gmail.com>
2023-11-18 18:28:17 -0300

    add css styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,68 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { StoreProvider } from '@/utils/Store'
+import { NextComponentType } from 'next'
+import { SessionProvider, useSession } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import 'react-toastify/dist/ReactToastify.css';
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & {auth?: {adminOnly:boolean}} // add auth type
+  adminOnly:boolean
+}
+
+interface AuthProps{
+  children:any;
+  adminOnly: boolean
+}
+
+
+
+
+export default function App({ Component, pageProps:{session, ...pageProps} }: CustomAppProps) {
+  return (
+    <>
+    <SessionProvider session={session}>
+    <StoreProvider>
+    
+      {Component.auth ? (
+        <Auth adminOnly={Component.auth.adminOnly}>
+          <div className='bg-icem'>
+            <Component {...pageProps} />
+          </div>
+        </Auth>
+      ):(
+        <div className='bg-icem'>
+          <Component {...pageProps} />
+        </div>
+      )}
+     
+        
+    </StoreProvider>
+    </SessionProvider>
+    </>
+  )
+}
+
+
+
+
+//component to filter logged user and redirect unlogged user to page/unauthorized.tsx 
+function Auth({ children, adminOnly }:AuthProps){
+  const router = useRouter();
+  const {status, data: session}:any = useSession({
+    required: true,
+    onUnauthenticated(){
+      router.push('/unauthorized?message=login required');
+    }
+  });
+  if(status === 'loading'){
+    return <div>Loading...</div>
+  }
+
+  if (adminOnly && !session.user.isAdmin){
+    router.push('/unauthorized?message=admin login required');
+  }
+
+  return children;
 }
