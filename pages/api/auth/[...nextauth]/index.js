490433732bdebd40daa01fdfce0ab6742e4d849e
vituhonorato <vituhonorato.89@gmail.com>
2023-11-18 19:06:18 -0300

    add next auth

---
+++ pages/api/auth/[...nextauth]/index.js
@@ -0,0 +1,57 @@
+import User from "@/models/User";
+import db from "@/utils/db";
+import NextAuth from "next-auth/next";
+let bcrypt = require('bcryptjs');
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+export default NextAuth({
+    session: {
+        strategy:'jwt',
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if(user?._id) token._id = user._id;
+            if(user?.isAdmin) token.isAdmin = user.isAdmin
+            return token;
+        },
+        async session({session, token}){
+            if(token?._id) session.user._id = token._id;
+            if(token?.isAdmin) session.user.isAdmin = token.isAdmin
+            return session;
+        },
+    
+    },
+    providers:[
+        CredentialsProvider({
+            async authorize(credentials) {
+                await db.connect();
+                const user = await User.findOne({
+                    email: credentials.email,
+                });
+                await db.disconnect();
+                if(user && bcrypt.compareSync(credentials.password, user.password)){
+                    return {
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        image: 'f',
+                        isAdmin: user.isAdmin,
+                    };
+                }
+                throw new Error('Invalid email or password');
+            }
+        }),
+         GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      })
+    ],
+   
+    
+})
