223ceb67277edef618e9400a76087305ec7a82c4
vituhonorato <vituhonorato.89@gmail.com>
2023-11-19 16:19:00 -0300

    fix css tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout"
+import Link from "next/link"
 
 
 
 const Home = () => {
   return (
    <Layout>
+    <Link href='javascript'>Javascript</Link>
+    <Link href='node'>Node</Link>
    </Layout>
   )
 }
