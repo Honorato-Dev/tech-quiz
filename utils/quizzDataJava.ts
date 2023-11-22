8462c9e886f622bcfa514541ee782244084f73cf
vituhonorato <vituhonorato.89@gmail.com>
2023-11-22 10:58:12 -0300

    add new java data

---
+++ utils/quizzDataJava.ts
@@ -0,0 +1,16 @@
+const quizDataJava = {
+
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+}
+
+   
+
+
