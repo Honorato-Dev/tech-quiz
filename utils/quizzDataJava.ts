513fbe8f25e5d13335201399f23f1123ac2c6bb8
vituhonorato <vituhonorato.89@gmail.com>
2023-11-22 11:11:32 -0300

    add new java questions structure

--- utils/quizzDataJava.ts
+++ utils/quizzDataJava.ts
@@ -1,5 +1,13 @@
-const quizDataJava = {
-
+const quizDataJava = [
+    {    question: "Ao implementar o encapsulamento, você não pode acessar diretamente as propriedades _ da classe, a menos que esteja escrevendo código dentro da própria classe.",
+    options: [
+      { text: "private", id: "a" },
+      { text: "protected", id: "b" },
+      { text: "no-modifier", id: "c" },
+      { text: "public", id: "d" },
+    ]
+},
+{
     question: "",
     options: [
       { text: "", id: "a" },
@@ -9,7 +17,90 @@ const quizDataJava = {
     ],
     correct: "",
 
-}
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+{
+    question: "",
+    options: [
+      { text: "", id: "a" },
+      { text: "", id: "b" },
+      { text: "", id: "c" },
+      { text: "", id: "d" },
+    ],
+    correct: "",
+
+},
+
+
+
+ 
+
+]
 
    
 
