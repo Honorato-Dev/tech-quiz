8a12e5d40725b8ebbe57982f1d244f9d1da405f2
vituhonorato <vituhonorato.89@gmail.com>
2023-11-24 18:42:32 -0300

    building main page and change tag p to h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,11 @@ import Link from "next/link"
 const Home = () => {
   return (
    <Layout>
-    <Link href='javascript'>Javascript</Link>
-    <Link href='node'>Node</Link>
+     <div className="flex justify-center mx-auto ">
+      <h1>Bem-vindo ao Tech Quiz: Explore o Universo da Tecnologia!</h1>
+      <h2></h2>
+
+     </div>
    </Layout>
   )
 }
