f39fd06a0131ee1af9dbdbb1c6e0abd9c53dcc88
vituhonorato <vituhonorato.89@gmail.com>
2023-11-24 18:47:21 -0300

    add respondive titlte

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
    <Layout>
      <div className="flex justify-center mx-auto ">
-      <h1>Bem-vindo ao Tech Quiz: Explore o Universo da Tecnologia!</h1>
+      <h1 className="text-base md:text-3xl lg:text-5xl font-bold text-white">Bem-vindo ao Tech Quiz: Explore o Universo da Tecnologia!</h1>
       <h2></h2>
 
      </div>
