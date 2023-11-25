64f32e6b61fd7e87e3b3060a331e82b45e580c48
vituhonorato <vituhonorato.89@gmail.com>
2023-11-25 12:52:53 -0300

    create get started page

---
+++ pages/getstarted.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const GetStarted = () => {
+  return (
+    <Layout title='Get Started'>
+
+    </Layout>
+  )
+}
+
+export default GetStarted
