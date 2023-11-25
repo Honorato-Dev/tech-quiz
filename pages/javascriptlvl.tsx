eef6a33da587fbf4ca108b027ef780dddb378042
vituhonorato <vituhonorato.89@gmail.com>
2023-11-25 12:44:50 -0300

    add new page javascript lvl

---
+++ pages/javascriptlvl.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const JavascriptLvl = () => {
+  return (
+    <Layout title='Javascript lvl'>
+      
+    </Layout>
+  )
+}
+
+export default JavascriptLvl
