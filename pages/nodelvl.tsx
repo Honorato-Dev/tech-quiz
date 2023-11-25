06b06b33373b8e1af91c1e0be4392955253aeb35
vituhonorato <vituhonorato.89@gmail.com>
2023-11-25 12:49:43 -0300

    add new page node lvl

---
+++ pages/nodelvl.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const NodeLvl = () => {
+  return (
+    <Layout title='Node lvl'>
+      
+    </Layout>
+  )
+}
+
+export default NodeLvl
