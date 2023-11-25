7f96050bbe9c55b9bd2740f49aa99e1638175cd1
vituhonorato <vituhonorato.89@gmail.com>
2023-11-24 23:21:28 -0300

    add github provider

--- pages/api/auth/[...nextauth]/index.js
+++ pages/api/auth/[...nextauth]/index.js
@@ -5,6 +5,7 @@ let bcrypt = require('bcryptjs');
 import  CredentialsProvider  from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
     session: {
@@ -50,6 +51,10 @@ export default NextAuth({
     FacebookProvider({
         clientId: process.env.FACEBOOK_CLIENT_ID,
         clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      }),
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
       })
     ],
    
