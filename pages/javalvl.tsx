e08c811de07a4e7bdbacc8c8df9d003c97249152
vituhonorato <vituhonorato.89@gmail.com>
2023-11-25 12:47:34 -0300

    add new page java lvl

---
+++ pages/javalvl.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const JavaLvl = () => {
+  return (
+    <Layout title='Java lvl'>
+      
+    </Layout>
+  )
+}
+
+export default JavaLvl
