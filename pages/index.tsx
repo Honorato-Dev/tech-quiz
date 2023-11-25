06113f85fa7baa927563bcb802aef453b9140c1c
vituhonorato <vituhonorato.89@gmail.com>
2023-11-24 22:54:41 -0300

    add text in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,15 @@ const Home = () => {
   return (
    <Layout>
      <div className="flex justify-center mx-auto ">
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-col justify-center">
 
           <div className={alfa.className}>
-        <h1 className="text-xl md:text-3xl lg:text-5xl text-center  text-white ">Bem-vindo ao Tech Quiz!</h1>
-        <h2 className="text-xl md:text-3xl lg:text-5xl text-center text-white "> Explore o Universo da Tecnologia.</h2>
+        <h1 className="text-2xl md:text-3xl lg:text-5xl text-center  text-white ">Bem-vindo ao Tech Quiz!</h1>
+        <h2 className="text-2xl md:text-3xl lg:text-5xl text-center text-white mb-10"> Explore o Universo da Tecnologia.</h2>
       </div>
+      <p className="text-lg md:text-xl lg:text-2xl text-center  text-white mt-11">
+      No Tech Quiz, sua jornada pelo vasto universo da tecnologia começa aqui! Somos um destino online que se aprofunda nas profundezas do conhecimento digital, oferecendo um espaço dinâmico e interativo para testar suas habilidades nas principais linguagens de tecnologia utilizadas atualmente. Este é o lugar onde entusiastas, profissionais da área e curiosos se encontram para explorar, aprender e aprimorar suas habilidades no mundo digital.
+      </p>
       </div>
     
       
