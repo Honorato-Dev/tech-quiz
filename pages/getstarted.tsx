9b61391c145e046aac88c4955cfae22ff1604454
vituhonorato <vituhonorato.89@gmail.com>
2023-11-27 17:40:00 -0300

    add React Icon

--- pages/getstarted.tsx
+++ pages/getstarted.tsx
@@ -1,12 +1,16 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import Layout from '@/components/Layout';
+import React from 'react';
 
 const GetStarted = () => {
   return (
-    <Layout title='Get Started'>
-
+    <Layout title="Get Started">
+      <div className="flex h-screen flex-col justify-center">
+        <div className="flex flex-row justify-center">
+          
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default GetStarted
+export default GetStarted;
