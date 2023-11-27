4447bf7e925e1b776978bfac918eb3b71a4c8f06
vituhonorato <vituhonorato.89@gmail.com>
2023-11-27 18:32:00 -0300

    add tech responsive layout

--- pages/getstarted.tsx
+++ pages/getstarted.tsx
@@ -16,15 +16,15 @@ const GetStarted = () => {
     <Layout title="Get Started">
       <div className="flex  flex-col justify-center">
         <div className="flex flex-row justify-center">
-          <div className='flex '><SiJavascript className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><SiNodedotjs className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><FaJava className='w-24 h-24 mx-5' /></div>
+          <div className='flex '><SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><SiNodedotjs className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><FaJava className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
           
-          <div className='flex'><SiSpring className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><SiPython className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><SiDjango className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><SiPhp className='w-24 h-24 mx-5' /></div>
-          <div className='flex'><TbBrandLaravel className='w-24 h-24 mx-5'/></div>
+          <div className='flex'><SiSpring className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><SiPython className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><SiDjango className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><SiPhp className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5' /></div>
+          <div className='flex'><TbBrandLaravel className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 mx-5'/></div>
           <div className='flex'></div>
         </div>
       </div>
