74f0a450d1692477601dab9904e5d68d1ac372f6
vituhonorato <vituhonorato.89@gmail.com>
2023-11-27 18:27:09 -0300

    add tech logo in get started page

--- pages/getstarted.tsx
+++ pages/getstarted.tsx
@@ -2,6 +2,12 @@ import Layout from '@/components/Layout';
 import React from 'react';
 import { SiJavascript } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
+import { SiNodedotjs } from "react-icons/si";
+import { SiSpring } from "react-icons/si";
+import { SiPython } from "react-icons/si";
+import { SiDjango } from "react-icons/si";
+import { SiPhp } from "react-icons/si";
+import { TbBrandLaravel } from "react-icons/tb";
 
 
 
@@ -10,10 +16,15 @@ const GetStarted = () => {
     <Layout title="Get Started">
       <div className="flex  flex-col justify-center">
         <div className="flex flex-row justify-center">
-          <div className='flex '><SiJavascript className='w-24 h-24' /></div>
-          <div className='flex'><FaJava className='w-24 h-24' /></div>
-          <div className='flex'></div>
-          <div className='flex'></div>
+          <div className='flex '><SiJavascript className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><SiNodedotjs className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><FaJava className='w-24 h-24 mx-5' /></div>
+          
+          <div className='flex'><SiSpring className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><SiPython className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><SiDjango className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><SiPhp className='w-24 h-24 mx-5' /></div>
+          <div className='flex'><TbBrandLaravel className='w-24 h-24 mx-5'/></div>
           <div className='flex'></div>
         </div>
       </div>
