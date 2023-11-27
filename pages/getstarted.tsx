b6f5f755a04440e293923546f5e244e2537004e1
vituhonorato <vituhonorato.89@gmail.com>
2023-11-27 17:51:40 -0300

    buiding get started page

--- pages/getstarted.tsx
+++ pages/getstarted.tsx
@@ -1,12 +1,20 @@
 import Layout from '@/components/Layout';
 import React from 'react';
+import { SiJavascript } from "react-icons/si";
+import { FaJava } from "react-icons/fa";
+
+
 
 const GetStarted = () => {
   return (
     <Layout title="Get Started">
-      <div className="flex h-screen flex-col justify-center">
+      <div className="flex  flex-col justify-center">
         <div className="flex flex-row justify-center">
-          
+          <div className='flex '><SiJavascript className='w-24 h-24' /></div>
+          <div className='flex'><FaJava className='w-24 h-24' /></div>
+          <div className='flex'></div>
+          <div className='flex'></div>
+          <div className='flex'></div>
         </div>
       </div>
     </Layout>
