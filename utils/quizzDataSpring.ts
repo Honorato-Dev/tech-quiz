190e10315512e655003f46e1c940d529eb433b41
vituhonorato <vituhonorato.89@gmail.com>
2023-11-29 13:37:14 -0300

    add quizzDataSpring

---
+++ utils/quizzDataSpring.ts
@@ -0,0 +1,44 @@
+const quizzDataSpring = [
+    {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+    //   {
+    //     question: "",
+    //     options: [
+    //       { text: "", id: "a" },
+    //       { text: "", id: "b" },
+    //       { text: "", id: "c" },
+    //       { text: "", id: "d" },
+    //     ],
+    //     correct: "",
+    //   }
+]
+
+export default quizzDataSpring
