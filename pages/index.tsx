85a9b72548af65247adbe346670ff71a51641c37
vituhonorato <vituhonorato.89@gmail.com>
2023-12-04 18:52:49 -0300

    change get started button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,50 @@
-import Layout from "@/components/Layout"
-import { Alfa_Slab_One } from "next/font/google"
-import Link from "next/link"
+import Layout from '@/components/Layout';
+import { Alfa_Slab_One } from 'next/font/google';
+import Link from 'next/link';
 
 const alfa = Alfa_Slab_One({
   subsets: ['latin'],
-    weight: ['400'],
-})
+  weight: ['400'],
+});
 
 const Home = () => {
- 
-
-
   return (
-   <Layout>
-     <div className="flex h-screen justify-center mx-auto ">
-      <div className="flex flex-col justify-center">
-
+    <Layout>
+      <div className="flex h-screen justify-center mx-auto ">
+        <div className="flex flex-col justify-center">
           <div className={alfa.className}>
-        <h1 className="text-2xl md:text-3xl lg:text-5xl text-center  text-white ">Bem-vindo ao Tech Quiz!</h1>
-        <h2 className="text-2xl md:text-3xl lg:text-5xl text-center text-white mb-10"> Explore o Universo da Tecnologia.</h2>
+            <h1 className="text-2xl md:text-3xl lg:text-5xl text-center  text-white ">
+              Bem-vindo ao Tech Quiz!
+            </h1>
+            <h2 className="text-2xl md:text-3xl lg:text-5xl text-center text-white mb-10">
+              {' '}
+              Explore o Universo da Tecnologia.
+            </h2>
+          </div>
+          <p className="text-lg md:text-xl lg:text-2xl text-center  text-white mt-11 mb-4">
+            No Tech Quiz, sua jornada pelo vasto universo da tecnologia começa
+            aqui! Somos um destino online que se aprofunda nas profundezas do
+            conhecimento digital, oferecendo um espaço dinâmico e interativo
+            para testar suas habilidades nas principais linguagens de tecnologia
+            utilizadas atualmente. Este é o lugar onde entusiastas,
+            profissionais da área e curiosos se encontram para explorar,
+            aprender e aprimorar suas habilidades no mundo digital.
+          </p>
+          <div className="text-center mt-20">
+            <Link
+              className="font-bold py-7 px-7 rounded-md bg-blue-500 text-white bg-blue-700;"
+              href="/getstarted"
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center"></div>
+
+        <h2></h2>
       </div>
-      <p className="text-lg md:text-xl lg:text-2xl text-center  text-white mt-11 mb-4">
-      No Tech Quiz, sua jornada pelo vasto universo da tecnologia começa aqui! Somos um destino online que se aprofunda nas profundezas do conhecimento digital, oferecendo um espaço dinâmico e interativo para testar suas habilidades nas principais linguagens de tecnologia utilizadas atualmente. Este é o lugar onde entusiastas, profissionais da área e curiosos se encontram para explorar, aprender e aprimorar suas habilidades no mundo digital.
-      </p>
-      <div className="text-center mt-14">
-      <Link className="font-bold py-7 px-7 rounded-md bg-blue-500 text-white bg-blue-700;" href='/getstarted'>Get Started</Link>
-     </div>
-      </div>
-      <div className="flex flex-col justify-center">
+    </Layout>
+  );
+};
 
-      </div>
-  
-      
-      <h2></h2>
-
-     </div>
-   </Layout>
-  )
-}
-
-
-export default Home
+export default Home;
