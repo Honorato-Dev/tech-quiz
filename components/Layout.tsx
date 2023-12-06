d6ace848024bd562afcbd21c62e72a295d3b231a
vituhonorato <vituhonorato.89@gmail.com>
2023-12-06 14:33:54 -0300

    change layout footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -209,7 +209,7 @@ const Layout = ({ children, title }: any) => {
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div className="sm:flex sm:items-center sm:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-900">
                 © 2023{' '}
                 <a href="https://flowbite.com/" className="hover:underline">
                   Tech Quiz™
