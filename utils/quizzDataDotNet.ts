7a50ae1c99b90661d952b2cd3f4216dc3b86fbe4
vituhonorato <vituhonorato.89@gmail.com>
2023-12-07 12:37:05 -0300

    add quizzDataDotNet

---
+++ utils/quizzDataDotNet.ts
@@ -0,0 +1,93 @@
+const quizDataDotNet = [
+    {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+    //   {
+    //     question: "",
+    //     image:"",
+    //     options: [
+    //       { text: "", id: "a" },
+    //       { text: "", id: "b" },
+    //       { text: "", id: "c" },
+    //       { text: "", id: "d" },
+    //     ],
+    //     correct: "",
+    //   },
+
+]
+export default quizDataDotNet
