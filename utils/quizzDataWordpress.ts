dbb0e794d080b277cc103ab249b01ffe2f7eb288
vituhonorato <vituhonorato.89@gmail.com>
2023-12-07 12:14:01 -0300

    add quizzDataWordpress

---
+++ utils/quizzDataWordpress.ts
@@ -0,0 +1,93 @@
+const quizDataWordpress = [
+    {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        image:"",
+        options: [
+          { text: "--dev-tools", id: "a" },
+          { text: "--inspect", id: "b" },
+          { text: "--chrome", id: "c" },
+          { text: "--debug", id: "d" },
+        ],
+        correct: "b",
+      },
+
+    //   {
+    //     question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+    //     image:"",
+    //     options: [
+    //       { text: "--dev-tools", id: "a" },
+    //       { text: "--inspect", id: "b" },
+    //       { text: "--chrome", id: "c" },
+    //       { text: "--debug", id: "d" },
+    //     ],
+    //     correct: "b",
+    //   },
+]
+export default quizDataWordpress
