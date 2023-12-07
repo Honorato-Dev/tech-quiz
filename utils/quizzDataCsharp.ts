a0b2636b7b2ab8170c475be5c38e788aa44e479a
vituhonorato <vituhonorato.89@gmail.com>
2023-12-07 12:33:51 -0300

    add quizzDataCsharp

---
+++ utils/quizzDataCsharp.ts
@@ -0,0 +1,105 @@
+const quizDataCsharp = [
+    {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        image:"",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+    //   {
+    //     question: "",
+    //     image:"",
+    //     options: [
+    //       { text: "", id: "a" },
+    //       { text: "", id: "b" },
+    //       { text: "", id: "c" },
+    //       { text: "", id: "d" },
+    //     ],
+    //     correct: "",
+    //   },
+
+
+]
+export default quizDataCsharp
