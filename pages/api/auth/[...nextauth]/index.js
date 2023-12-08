fe7f7579d26c7359259f79b1d69f15478aef661f
vituhonorato <vituhonorato.89@gmail.com>
2023-12-08 08:42:02 -0300

    add linkedin auth

--- pages/api/auth/[...nextauth]/index.js
+++ pages/api/auth/[...nextauth]/index.js
@@ -55,6 +55,10 @@ export default NextAuth({
       GitHubProvider({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET
+      }),
+      LinkedInProvider({
+        clientId: process.env.LINKEDIN_CLIENT_ID,
+        clientSecret: process.env.LINKEDIN_CLIENT_SECRET
       })
     ],
    
