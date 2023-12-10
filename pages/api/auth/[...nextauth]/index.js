d2ecb63f306de068eb1d96278afd6e87885efbf8
vituhonorato <vituhonorato.89@gmail.com>
2023-12-10 19:48:14 -0300

    remold login access login

--- pages/api/auth/[...nextauth]/index.js
+++ pages/api/auth/[...nextauth]/index.js
@@ -4,9 +4,8 @@ import NextAuth from "next-auth/next";
 let bcrypt = require('bcryptjs');
 import  CredentialsProvider  from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 import GitHubProvider from "next-auth/providers/github";
-import LinkedInProvider from "next-auth/providers/linkedin";
+
 
 export default NextAuth({
     session: {
@@ -49,18 +48,12 @@ export default NextAuth({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      }),
+   
       GitHubProvider({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET
       }),
-      LinkedInProvider({
-        clientId: process.env.LINKEDIN_CLIENT_ID,
-        clientSecret: process.env.LINKEDIN_CLIENT_SECRET
-      })
+    
     ],
    
     
