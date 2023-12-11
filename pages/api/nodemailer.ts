fa7a26ecc8330ad8e4eabab97159089354ef08c4
vituhonorato <vituhonorato.89@gmail.com>
2023-12-11 13:56:23 -0300

    add nodemailer.ts2

--- pages/api/nodemailer.ts
+++ pages/api/nodemailer.ts
@@ -1,9 +1,9 @@
-import nodemailer from "nodemailer";
+const  nodemailer = require("nodemailer");
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 
-export const transporter = nodemailer.createTransport({
+ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: email,
@@ -11,7 +11,9 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-export const mailOptions = {
+ const mailOptions = {
   from: email,
   to: email,
 };
+
+export {mailOptions, transporter}
