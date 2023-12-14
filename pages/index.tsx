879dd812ac42ad45bf59847ba71106de518b2b82
vituhonorato <vituhonorato.89@gmail.com>
2023-12-14 16:10:55 -0300

    rebuilding layout from get started 7

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,47 +38,102 @@ const Home = () => {
               Get Started
             </Link>
           </div>
-          
         </div>
-        
-        <div className="flex flex-col   lg:flex-row  lg:space-x-16 justify-center">
-          
-            <div className="   bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
-              
-              <p className='flex mt-20  text-white font-semibold text-base lg:text-2xl'>Responder a um questionário de tecnologia pode oferecer uma série de vantagens significativas no contexto da evolução de carreira. Em um mundo cada vez mais orientado pela inovação e digitalização, demonstrar habilidades e conhecimentos tecnológicos é essencial para o crescimento profissional. Ao participar ativamente de questionários sobre tecnologia, os profissionais podem colher benefícios como:
-              </p>
-              
-            </div>
-            <div className=" p-4 space-y-3 bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px]">
-              <p className='flex font-semibold p-1 text-sm lg:text-base'><span className={alfa.className}><span className='mr-4 text-lg lg:text-xl text-white font-normal '>Atualização de Conhecimentos:</span></span> Ao responder perguntas sobre tecnologia, os profissionais são desafiados a revisar e atualizar seus conhecimentos. Isso é vital em um cenário onde a tecnologia evolui rapidamente, garantindo que as habilidades estejam alinhadas com as demandas do mercado.
 
-              </p>
-              <p className='flex font-semibold p-1 text-sm lg:text-base'><span className={alfa.className}><span className='mr-4 text-lg lg:text-xl text-white font-normal '>Destaque no Mercado:</span></span>
-              Participar ativamente de questionários de tecnologia pode destacar um profissional no mercado de trabalho. Isso demonstra proatividade, interesse em aprender e adaptabilidade, características valorizadas por empregadores que buscam talentos capazes de acompanhar as mudanças tecnológicas.
-              </p>
-              <p className='flex font-semibold p-1 text-sm lg:text-base'><span className={alfa.className}><span className='mr-4 text-lg lg:text-xl text-white font-normal '>Networking e Oportunidades:</span></span>
-               Muitos questionários de tecnologia são promovidos por comunidades e empresas do setor. Ao participar, os profissionais têm a chance de expandir sua rede de contatos, interagindo com colegas, líderes e especialistas. Isso pode resultar em novas oportunidades de emprego, colaborações e parcerias.
-              </p>
-            
-              <p></p>
-            </div>
-          
-        </div>
         <div className="flex flex-col   lg:flex-row  lg:space-x-16 justify-center">
-        <div className=" bg-white p-4  bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
-        <p className='flex font-semibold p-1 text-sm lg:text-base'><span className=''></span>
-              Certificação Informal: Responder a questionários de tecnologia pode servir como uma forma de certificação informal. Os resultados obtidos podem ser compartilhados em perfis profissionais online, como o LinkedIn, evidenciando o nível de conhecimento e competência do profissional em determinadas áreas tecnológicas.
-              </p>
-              <p className='flex font-semibold p-1 text-sm lg:text-base'> <span className=''></span>Autoavaliação e Desenvolvimento Pessoal: Os questionários proporcionam uma oportunidade para autoavaliação. Ao identificar áreas de conhecimento que precisam de aprimoramento, os profissionais podem criar planos de desenvolvimento pessoal direcionados, focando em melhorar habilidades específicas.</p>
-              <p className='flex font-semibold p-1 text-sm lg:text-base'><span className=''></span>
-              Acesso a Recursos Educacionais: Muitos questionários de tecnologia estão associados a recursos educacionais, como webinars, cursos online e material de estudo. Ao participar desses questionários, os profissionais podem acessar oportunidades adicionais de aprendizado que contribuem para sua formação contínua.
-              </p>
-        </div>
-        <div className=" bg-white  bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
+          <div className="   bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
+            <p className="flex mt-20  text-white font-semibold text-base lg:text-2xl">
+              Responder a um questionário de tecnologia pode oferecer uma série
+              de vantagens significativas no contexto da evolução de carreira.
+              Em um mundo cada vez mais orientado pela inovação e digitalização,
+              demonstrar habilidades e conhecimentos tecnológicos é essencial
+              para o crescimento profissional. Ao participar ativamente de
+              questionários sobre tecnologia, os profissionais podem colher
+              benefícios como:
+            </p>
+          </div>
+          <div className=" p-4 space-y-3 bg-opacity-70 w-[] h-[650px] mt-4 rounded-md lg:w-[680px] lg:h-[450px]">
+            <p className="flex font-semibold p-1 text-sm lg:text-base">
+              <span className={alfa.className}>
+                <span className="mr-4 text-lg lg:text-xl text-white font-normal ">
+                  Atualização de Conhecimentos:
+                </span>
+              </span>{' '}
+              Ao responder perguntas sobre tecnologia, os profissionais são
+              desafiados a revisar e atualizar seus conhecimentos. Isso é vital
+              em um cenário onde a tecnologia evolui rapidamente, garantindo que
+              as habilidades estejam alinhadas com as demandas do mercado.
+            </p>
+            <p className="flex font-semibold p-1 text-sm lg:text-base">
+              <span className={alfa.className}>
+                <span className="mr-4 text-lg lg:text-xl text-white font-normal ">
+                  Destaque no Mercado:
+                </span>
+              </span>
+              Participar ativamente de questionários de tecnologia pode destacar
+              um profissional no mercado de trabalho. Isso demonstra
+              proatividade, interesse em aprender e adaptabilidade,
+              características valorizadas por empregadores que buscam talentos
+              capazes de acompanhar as mudanças tecnológicas.
+            </p>
+            <p className="flex font-semibold p-1 text-sm lg:text-base">
+              <span className={alfa.className}>
+                <span className="mr-4 text-lg lg:text-xl text-white font-normal ">
+                  Networking e Oportunidades:
+                </span>
+              </span>
+              Muitos questionários de tecnologia são promovidos por comunidades
+              e empresas do setor. Ao participar, os profissionais têm a chance
+              de expandir sua rede de contatos, interagindo com colegas, líderes
+              e especialistas. Isso pode resultar em novas oportunidades de
+              emprego, colaborações e parcerias.
+            </p>
 
+            <p></p>
+          </div>
         </div>
+        <div className="flex flex-col mt-10 lg:mt-4   lg:flex-row  lg:space-x-16 justify-center">
+          <div className="  p-4  bg-opacity-70 w-[] h-[650px]  lg:mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
+            <p className="flex font-semibold pl-1 ml-1 text-sm lg:text-base">
+              <span className={alfa.className}>
+                <span className="flex mr-4 pr-2 text-lg lg:text-xl text-white font-normal ">
+                  Certificação Informal:
+                </span>
+              </span>
+              Responder a questionários de tecnologia pode servir como uma forma
+              de certificação informal. Os resultados obtidos podem ser
+              compartilhados em perfis profissionais online, como o LinkedIn,
+              evidenciando o nível de conhecimento e competência do profissional
+              em determinadas áreas tecnológicas.
+            </p>
+            <p className="flex font-semibold p-1 text-sm lg:text-base">
+              {' '}
+              <span className={alfa.className}>
+                <span className="flex mr-4 text-lg lg:text-xl text-white font-normal ">
+                  Autoavaliação e Desenvolvimento Pessoal:
+                </span>
+              </span>{' '}
+              Os questionários proporcionam uma oportunidade para autoavaliação.
+              Ao identificar áreas de conhecimento que precisam de
+              aprimoramento, os profissionais podem criar planos de
+              desenvolvimento pessoal direcionados, focando em melhorar
+              habilidades específicas.
+            </p>
+            <p className="flex font-semibold p-1 text-sm lg:text-base">
+              <span className={alfa.className}>
+                <span className="flex mr-4 text-lg lg:text-xl text-white font-normal ">
+                  Acesso a Recursos Educacionais:
+                </span>
+              </span>
+              Muitos questionários de tecnologia estão associados a recursos
+              educacionais, como webinars, cursos online e material de estudo.
+              Ao participar desses questionários, os profissionais podem acessar
+              oportunidades adicionais de aprendizado que contribuem para sua
+              formação contínua.
+            </p>
+          </div>
+          <div className=" bg-white  bg-opacity-70 w-[] h-[650px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] "></div>
         </div>
-        
       </div>
     </Layout>
   );
