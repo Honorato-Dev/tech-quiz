fcfc673c5ceedcbc0e4430b916ec8adca39189d7
vituhonorato <vituhonorato.89@gmail.com>
2023-12-13 21:55:32 -0300

    building layout in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
               Explore o Universo da Tecnologia.
             </h2>
           </div>
-          <p className="text-lg md:text-xl lg:text-2xl text-center  text-white mt-11 mb-4">
+          <p className="text-lg md:text-xl lg:text-2xl font-semibold text-center  text-white mt-11 mb-4">
             No Tech Quiz, sua jornada pelo vasto universo da tecnologia começa
             aqui! Somos um destino online que se aprofunda nas profundezas do
             conhecimento digital, oferecendo um espaço dinâmico e interativo
@@ -41,10 +41,17 @@ const Home = () => {
           
         </div>
         
-        <div className="flex flex-col  lg:flex-row  lg:space-x-16 justify-center">
+        <div className="flex flex-col   lg:flex-row  lg:space-x-16 justify-center">
           
-            <div className="bg-white bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">teste</div>
-            <div className="bg-white bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px]">teste</div>
+            <div className="bg-white   bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px] ">
+              
+              <p className='flex mt-24  text-white font-semibold text-sm lg:text-2xl'>Responder a um questionário de tecnologia pode oferecer uma série de vantagens significativas no contexto da evolução de carreira. Em um mundo cada vez mais orientado pela inovação e digitalização, demonstrar habilidades e conhecimentos tecnológicos é essencial para o crescimento profissional. Ao participar ativamente de questionários sobre tecnologia, os profissionais podem colher benefícios como:
+              </p>
+              
+            </div>
+            <div className="bg-white bg-opacity-70 w-[] h-[350px] mt-4 rounded-md lg:w-[680px] lg:h-[450px]">
+              <p></p>
+            </div>
           
         </div>
         
