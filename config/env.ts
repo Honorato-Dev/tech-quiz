62f34b40b5ee5f24c33313f0aa566853f466e2a3
vituhonorato <vituhonorato.89@gmail.com>
2023-12-15 18:52:34 -0300

    add api/auth/forgot-passaword

--- config/env.ts
+++ config/env.ts
@@ -1,9 +1,10 @@
 class Env {
-    static SMPTP_HOST: string = process.env.SMPTP_HOST!;
-    static SMPTP_PORT: string = process.env.SMPTP_PORT!;
-    static SMPTP_USER: string = process.env.SMPTP_USER!;
-    static SMPTP_PASSWORD: string = process.env.SMPTP_PASSWORD!;
-    static SMPTP_SECURE: string = process.env.SMPTP_SECURE!;
+    static SMTP_HOST: string = process.env.SMTP_HOST!;
+    static SMTP_PORT: string = process.env.SMTP_PORT!;
+    static SMTP_USER: string = process.env.SMTP_USER!;
+    static SMTP_PASSWORD: string = process.env.SMTP_PASSWORD!;
+    static SMTP_SECURE: string = process.env.SMTP_SECURE!;
+    static EMAIL_FROM: string = process.env.EMAIL_FROM!;
 }
 
 export default Env
--- config/mail.ts
+++ config/mail.ts
@@ -2,12 +2,30 @@ const nodemailer = require("nodemailer");
 import Env from '@/config/env'
 
 export const transporter = nodemailer.createTransport({
-    host: Env.SMPTP_HOST,
-    port: Env.SMPTP_PORT,
-    secure: Env.SMPTP_SECURE,
+    host: Env.SMTP_HOST,
+    port: Number(Env.SMTP_PORT),
+    secure: Env.SMTP_SECURE,
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: Env.SMPTP_USER,
-      pass: Env.SMPTP_PASSWORD,
+      user: Env.SMTP_USER,
+      pass: Env.SMTP_PASSWORD,
     },
   });
+
+  //TO send the email
+
+  export  const  sendEmail = async (
+
+     to:string,
+ subject:string,
+ html:string,
+  ) : Promise<string|null> => {
+    const info = await transporter.sendEmail({
+    from:Env.EMAIL_FROM,
+    subject:subject,
+    html:html
+    })
+    return info?.messageId;
+  }
+ 
+  
