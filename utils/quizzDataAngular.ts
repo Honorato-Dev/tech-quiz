3c72afe44beb36de3d73f181e64954bd7401c6a3
vituhonorato <vituhonorato.89@gmail.com>
2024-03-07 12:17:37 -0300

    add new questino in angular

--- utils/quizzDataAngular.ts
+++ utils/quizzDataAngular.ts
@@ -11,13 +11,13 @@ const quizDataAngular = [
         correct: "b",
       },
       {
-        question: "Qual opção CLI você pode usar para depurar um script de nó no Chrome DevTools?",
+        question: "Qual a sintaxe utilizada para fazer interpolação de texto no Angular?",
         image:"",
         options: [
-          { text: "--dev-tools", id: "a" },
-          { text: "--inspect", id: "b" },
-          { text: "--chrome", id: "c" },
-          { text: "--debug", id: "d" },
+          { text: "{{ nomeVariavel }}", id: "a" },
+          { text: "[( nomeVariavel )]", id: "b" },
+          { text: "[ nomeVariavel ]", id: "c" },
+          { text: "[( ngModel )]", id: "d" },
         ],
         correct: "b",
       },
