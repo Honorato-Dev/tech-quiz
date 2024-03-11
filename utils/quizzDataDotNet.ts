b8c7997ec832ce52689b476f55bd7ca053c06998
vituhonorato <vituhonorato.89@gmail.com>
2024-03-11 17:34:55 -0300

    add dotNet questions

--- utils/quizzDataDotNet.ts
+++ utils/quizzDataDotNet.ts
@@ -1,82 +1,229 @@
 const quizDataDotNet = [
-    {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
+  {
+    question: "O que é o .NET Framework?",
+    image: "",
+    options: [
+      { text: "Uma plataforma de desenvolvimento de software da Microsoft.", id: "a" },
+      { text: "Um banco de dados da Microsoft.", id: "b" },
+      { text: "Um sistema operacional da Microsoft.", id: "c" },
+      { text: "Uma linguagem de programação da Microsoft.", id: "d" }
+    ],
+    correct: "a"
+},
+{
+  question: "Qual linguagem de programação é amplamente utilizada com o .NET Framework?",
+  image: "",
+  options: [
+    { text: "C#", id: "a" },
+    { text: "Java", id: "b" },
+    { text: "Python", id: "c" },
+    { text: "Ruby", id: "d" }
+  ],
+  correct: "a"
+},
 
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
+{
+  question: "O que é o CLR (Common Language Runtime) do .NET Framework?",
+  image: "",
+  options: [
+    { text: "O ambiente de tempo de execução que gerencia o código .NET durante sua execução.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+}
+,
+{
+  question: "O que é o BCL (Base Class Library) do .NET Framework?",
+  image: "",
+  options: [
+    { text: "Um conjunto de classes reutilizáveis que fornecem funcionalidades comuns para aplicativos .NET.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o CIL (Common Intermediate Language) do .NET Framework?",
+  image: "",
+  options: [
+    { text: "Uma linguagem de programação intermediária que é compilada a partir do código fonte e posteriormente convertida em código nativo.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Um sistema operacional da Microsoft.", id: "c" },
+    { text: "Um conjunto de classes reutilizáveis que fornecem funcionalidades comuns para aplicativos .NET.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o principal objetivo do .NET Framework?",
+  image: "",
+  options: [
+    { text: "Facilitar o desenvolvimento de aplicativos robustos e escaláveis.", id: "a" },
+    { text: "Facilitar o desenvolvimento de aplicativos para dispositivos móveis.", id: "b" },
+    { text: "Facilitar o desenvolvimento de jogos.", id: "c" },
+    { text: "Facilitar o desenvolvimento de aplicativos desktop.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é a linguagem principal utilizada para desenvolvimento de aplicativos .NET?",
+  image: "",
+  options: [
+    { text: "C#", id: "a" },
+    { text: "Java", id: "b" },
+    { text: "Python", id: "c" },
+    { text: "C++", id: "d" }
+  ],
+  correct: "a"
+},
+
+{
+  question: "O que é o ASP.NET?",
+  image: "",
+  options: [
+    { text: "Um framework para construção de aplicativos web.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o Entity Framework?",
+  image: "",
+  options: [
+    { text: "Um ORM (Object-Relational Mapper) para o .NET Framework.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o .NET Core?",
+  image: "",
+  options: [
+    { text: "Uma implementação do .NET Framework para plataformas não-Windows.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o nome do CLI (Command-Line Interface) do .NET Core?",
+  image: "",
+  options: [
+    { text: "dotnet", id: "a" },
+    { text: "netcli", id: "b" },
+    { text: "dotcli", id: "c" },
+    { text: "corecli", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o .NET Standard?",
+  image: "",
+  options: [
+    { text: "Uma especificação que descreve APIs comuns entre todas as implementações do .NET.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o NuGet?",
+  image: "",
+  options: [
+    { text: "Um gerenciador de pacotes para o .NET Framework.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o .NET Framework Core?",
+  image: "",
+  options: [
+    { text: "O framework de código aberto para construção de aplicativos web, IoT e serviços em nuvem.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o .NET Standard Library?",
+  image: "",
+  options: [
+    { text: "Uma especificação que descreve APIs comuns entre todas as implementações do .NET.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o WPF (Windows Presentation Foundation)?",
+  image: "",
+  options: [
+    { text: "Um framework para criar aplicativos de desktop com uma interface gráfica rica.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o Xamarin?",
+  image: "",
+  options: [
+    { text: "Uma plataforma para desenvolvimento de aplicativos móveis usando C# e .NET.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+
+{
+    question: "O que é o ASP.NET Core?",
+    image: "",
+    options: [
+      { text: "Uma estrutura para construção de aplicativos web de alto desempenho.", id: "a" },
+      { text: "Um banco de dados da Microsoft.", id: "b" },
+      { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+      { text: "Um sistema operacional da Microsoft.", id: "d" }
+    ],
+    correct: "a"
+},
+{
+  question: "O que é o ASP.NET MVC?",
+  image: "",
+  options: [
+    { text: "Um framework para construção de aplicativos web baseados no padrão MVC (Model-View-Controller).", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é o Blazor?",
+  image: "",
+  options: [
+    { text: "Um framework para construção de aplicativos web interativos usando C# e .NET.", id: "a" },
+    { text: "Um banco de dados da Microsoft.", id: "b" },
+    { text: "Uma linguagem de programação da Microsoft.", id: "c" },
+    { text: "Um sistema operacional da Microsoft.", id: "d" }
+  ],
+  correct: "a"
+},
+
     //   {
     //     question: "",
     //     image:"",
