d81c86052102814dbf272ab1a3563f1f839b31be
vituhonorato <vituhonorato.89@gmail.com>
2024-03-11 18:33:59 -0300

    add python questions

--- utils/quizzDataPython.ts
+++ utils/quizzDataPython.ts
@@ -1,59 +1,205 @@
 const quizDataPython = [
-    {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "",
-      },
+  {
+    question: "Qual é o significado de Python?",
+    options: [
+        { text: "Uma serpente", id: "a" },
+        { text: "Uma linguagem de programação", id: "b" },
+        { text: "Um tipo de café", id: "c" },
+        { text: "Uma cidade na Europa", id: "d" }
+    ],
+    correct: "b"
+},
+{
+  question: "Qual é o caracter usado para indicar um comentário em Python?",
+  options: [
+      { text: "//", id: "a" },
+      { text: "#", id: "b" },
+      { text: "'", id: "c" },
+      { text: "//", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é a função usada para imprimir texto no Python?",
+  options: [
+      { text: "print()", id: "a" },
+      { text: "echo()", id: "b" },
+      { text: "display()", id: "c" },
+      { text: "show()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é a função utilizada para obter a entrada do usuário no Python?",
+  options: [
+      { text: "input()", id: "a" },
+      { text: "get_input()", id: "b" },
+      { text: "read()", id: "c" },
+      { text: "user_input()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para converter um objeto em uma string no Python?",
+  options: [
+      { text: "to_string()", id: "a" },
+      { text: "str()", id: "b" },
+      { text: "convert_string()", id: "c" },
+      { text: "stringify()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é o operador usado para verificar se dois valores são iguais em Python?",
+  options: [
+      { text: "==", id: "a" },
+      { text: "=", id: "b" },
+      { text: "===", id: "c" },
+      { text: "!=", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o operador usado para verificar se um valor está contido em uma lista em Python?",
+  options: [
+      { text: "in", id: "a" },
+      { text: "exists", id: "b" },
+      { text: "contains", id: "c" },
+      { text: "has", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para adicionar um elemento a uma lista em Python?",
+  options: [
+      { text: "add()", id: "a" },
+      { text: "append()", id: "b" },
+      { text: "insert()", id: "c" },
+      { text: "push()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é o método utilizado para remover um elemento de uma lista em Python?",
+  options: [
+      { text: "remove()", id: "a" },
+      { text: "delete()", id: "b" },
+      { text: "pop()", id: "c" },
+      { text: "erase()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o tipo de dados que representa uma sequência de caracteres em Python?",
+  options: [
+      { text: "string", id: "a" },
+      { text: "char", id: "b" },
+      { text: "text", id: "c" },
+      { text: "str", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é o método utilizado para retornar o comprimento de uma lista em Python?",
+  options: [
+      { text: "count()", id: "a" },
+      { text: "length()", id: "b" },
+      { text: "size()", id: "c" },
+      { text: "len()", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é a função utilizada para criar uma lista de números em Python?",
+  options: [
+      { text: "range()", id: "a" },
+      { text: "list()", id: "b" },
+      { text: "create_list()", id: "c" },
+      { text: "make_list()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para ordenar uma lista em Python?",
+  options: [
+      { text: "sort()", id: "a" },
+      { text: "order()", id: "b" },
+      { text: "arrange()", id: "c" },
+      { text: "sorted()", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é a função utilizada para criar uma função em Python?",
+  options: [
+      { text: "def()", id: "a" },
+      { text: "function()", id: "b" },
+      { text: "create_function()", id: "c" },
+      { text: "make_function()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é a função utilizada para retornar o valor absoluto de um número em Python?",
+  options: [
+      { text: "absolute()", id: "a" },
+      { text: "abs()", id: "b" },
+      { text: "absolute_value()", id: "c" },
+      { text: "value()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é a função utilizada para encontrar o valor máximo em um conjunto de números em Python?",
+  options: [
+      { text: "max()", id: "a" },
+      { text: "maximum()", id: "b" },
+      { text: "maximum_value()", id: "c" },
+      { text: "find_max()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o operador utilizado para realizar uma operação de exponenciação em Python?",
+  options: [
+      { text: "**", id: "a" },
+      { text: "^", id: "b" },
+      { text: "*", id: "c" },
+      { text: "^^", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é a função utilizada para converter um número em uma string em Python?",
+  options: [
+      { text: "string()", id: "a" },
+      { text: "str()", id: "b" },
+      { text: "convert_to_string()", id: "c" },
+      { text: "to_string()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é a função utilizada para converter uma string em um número em Python?",
+  options: [
+      { text: "int()", id: "a" },
+      { text: "convert_to_int()", id: "b" },
+      { text: "integer()", id: "c" },
+      { text: "to_int()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para remover espaços em branco do início e do final de uma string em Python?",
+  options: [
+      { text: "strip()", id: "a" },
+      { text: "remove_spaces()", id: "b" },
+      { text: "trim()", id: "c" },
+      { text: "clean()", id: "d" }
+  ],
+  correct: "a"
+},
+
     //   {
     //     question: "",
     //     image:"",
