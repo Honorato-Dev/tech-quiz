49beaa2a4cd5e241d2b492b27f4a5fa5b4f5e432
vituhonorato <vituhonorato.89@gmail.com>
2024-03-11 18:38:32 -0300

    add react questions

--- utils/quizzDataReact.ts
+++ utils/quizzDataReact.ts
@@ -1,81 +1,207 @@
 const quizDataReact = [
-    {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
+  {
+    question: "Qual é a biblioteca JavaScript de código aberto mais popular para a construção de interfaces de usuário?",
+    options: [
+        { text: "Vue.js", id: "a" },
+        { text: "Angular", id: "b" },
+        { text: "React", id: "c" },
+        { text: "Ember.js", id: "d" }
+    ],
+    correct: "c"
+},
+{
+  question: "Qual é o conceito central do React para a construção de interfaces de usuário?",
+  options: [
+      { text: "Componentes", id: "a" },
+      { text: "Diretivas", id: "b" },
+      { text: "Rotas", id: "c" },
+      { text: "Modelos", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é a sintaxe usada para renderizar um componente no React?",
+  options: [
+      { text: "<Componente />", id: "a" },
+      { text: "{ Componente }", id: "b" },
+      { text: "[Componente]", id: "c" },
+      { text: "(Componente)", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para atualizar o estado de um componente no React?",
+  options: [
+      { text: "this.setState()", id: "a" },
+      { text: "this.updateState()", id: "b" },
+      { text: "this.changeState()", id: "c" },
+      { text: "this.modifyState()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o hook usado para adicionar estado a um componente funcional no React?",
+  options: [
+      { text: "useState()", id: "a" },
+      { text: "useEffect()", id: "b" },
+      { text: "useContext()", id: "c" },
+      { text: "useReducer()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para lidar com eventos no React?",
+  options: [
+      { text: "handleEvent()", id: "a" },
+      { text: "eventHandler()", id: "b" },
+      { text: "onEvent()", id: "c" },
+      { text: "onClick()", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é a ferramenta usada para criar um novo projeto React?",
+  options: [
+      { text: "create-react-app", id: "a" },
+      { text: "react-create", id: "b" },
+      { text: "new-react-app", id: "c" },
+      { text: "react-init", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o conceito usado para dividir uma interface de usuário em componentes independentes e reutilizáveis no React?",
+  options: [
+      { text: "Fragmentação", id: "a" },
+      { text: "Segmentação", id: "b" },
+      { text: "Componentização", id: "c" },
+      { text: "Divisão", id: "d" }
+  ],
+  correct: "c"
+}, 
+{
+  question: "Qual é a ferramenta usada para adicionar estilos CSS a um componente React?",
+  options: [
+      { text: "React Styles", id: "a" },
+      { text: "Styled Components", id: "b" },
+      { text: "CSS Reactor", id: "c" },
+      { text: "React Stylesheet", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é o método utilizado para fazer uma solicitação HTTP no React?",
+  options: [
+      { text: "axios()", id: "a" },
+      { text: "fetch()", id: "b" },
+      { text: "http()", id: "c" },
+      { text: "ajax()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é o componente React utilizado para renderizar uma lista de elementos com chaves únicas?",
+  options: [
+      { text: "<List />", id: "a" },
+      { text: "<ul />", id: "b" },
+      { text: "<ol />", id: "c" },
+      { text: "<Fragment />", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método usado para fazer com que um componente React interrompa a renderização desnecessária?",
+  options: [
+      { text: "shouldComponentUpdate()", id: "a" },
+      { text: "componentShouldUpdate()", id: "b" },
+      { text: "renderIf()", id: "c" },
+      { text: "updateComponent()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o componente usado para representar a lógica de negócios em um aplicativo React?",
+  options: [
+      { text: "Container", id: "a" },
+      { text: "Presenter", id: "b" },
+      { text: "Componente", id: "c" },
+      { text: "Smart Component", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é o hook usado para disparar efeitos secundários em um componente funcional React?",
+  options: [
+      { text: "useEffect()", id: "a" },
+      { text: "useState()", id: "b" },
+      { text: "useEffectHook()", id: "c" },
+      { text: "useEffectEffect()", id: "d" }
+  ],
+  correct: "a"
+},
+
+{
+  question: "Qual é a biblioteca de roteamento mais comumente usada para aplicativos React?",
+  options: [
+      { text: "react-router", id: "a" },
+      { text: "route-react", id: "b" },
+      { text: "react-navigation", id: "c" },
+      { text: "react-route", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método utilizado para criar uma referência a um elemento DOM no React?",
+  options: [
+      { text: "createRef()", id: "a" },
+      { text: "newRef()", id: "b" },
+      { text: "ref()", id: "c" },
+      { text: "makeRef()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método usado para adicionar uma chave única a uma lista de elementos no React?",
+  options: [
+      { text: "addKey()", id: "a" },
+      { text: "key()", id: "b" },
+      { text: "id()", id: "c" },
+      { text: "uniqueKey()", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é o hook utilizado para acessar o contexto em um componente funcional no React?",
+  options: [
+      { text: "useContext()", id: "a" },
+      { text: "useState()", id: "b" },
+      { text: "useEffect()", id: "c" },
+      { text: "useContextProvider()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o método usado para criar um portal no React?",
+  options: [
+      { text: "createPortal()", id: "a" },
+      { text: "newPortal()", id: "b" },
+      { text: "portal()", id: "c" },
+      { text: "makePortal()", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Qual é o conceito usado para manter o estado global em um aplicativo React?",
+  options: [
+      { text: "Contexto", id: "a" },
+      { text: "Estado compartilhado", id: "b" },
+      { text: "Redux", id: "c" },
+      { text: "Armazenamento global", id: "d" }
+  ],
+  correct: "c"
+}
+
+
     //   {
     //     question: "",
     //     image:"",
