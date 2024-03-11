711e896bd3b12fa8da0a7a0a016131b722f620fa
vituhonorato <vituhonorato.89@gmail.com>
2024-03-11 18:59:50 -0300

    add wordpress questions

--- utils/quizzDataWordpress.ts
+++ utils/quizzDataWordpress.ts
@@ -1,83 +1,206 @@
 const quizDataWordpress = [
-    {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
+  {
+    question: "Qual é a linguagem de programação principal usada no desenvolvimento do WordPress?",
+    options: [
+        { text: "PHP", id: "a" },
+        { text: "JavaScript", id: "b" },
+        { text: "Python", id: "c" },
+        { text: "Ruby", id: "d" }
+    ],
+    correct: "a"
+},
+{
+  question: "Qual é o banco de dados padrão usado pelo WordPress?",
+  options: [
+      { text: "MySQL", id: "a" },
+      { text: "SQLite", id: "b" },
+      { text: "PostgreSQL", id: "c" },
+      { text: "MongoDB", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é um tema no WordPress?",
+  options: [
+      { text: "Um tipo de postagem personalizada", id: "a" },
+      { text: "Um modelo de página estática", id: "b" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "c" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "d" }
+  ],
+  correct: "c"
+},
 
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-      {
-        question: "",
-        image:"",
-        options: [
-          { text: "", id: "a" },
-          { text: "", id: "b" },
-          { text: "", id: "c" },
-          { text: "", id: "d" },
-        ],
-        correct: "b",
-      },
-
+{
+  question: "Qual é o nome da API usada para interagir com o WordPress remotamente?",
+  options: [
+      { text: "RESTful API", id: "a" },
+      { text: "XML-RPC API", id: "b" },
+      { text: "JSON API", id: "c" },
+      { text: "SOAP API", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Qual é a função do arquivo .htaccess no WordPress?",
+  options: [
+      { text: "Gerenciar a instalação do WordPress", id: "a" },
+      { text: "Controlar as configurações de cache do navegador", id: "b" },
+      { text: "Definir regras de redirecionamento e segurança do site", id: "c" },
+      { text: "Armazenar informações de configuração do tema", id: "d" }
+  ],
+  correct: "c"
+},
+{
+  question: "O que é um plugin no contexto do WordPress?",
+  options: [
+      { text: "Um modelo de página personalizada", id: "a" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "b" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "c" },
+      { text: "Um tipo de postagem personalizada", id: "d" }
+  ],
+  correct: "c"
+},
+{
+  question: "Qual é a função do arquivo wp-config.php no WordPress?",
+  options: [
+      { text: "Definir as configurações do banco de dados e outras configurações do site", id: "a" },
+      { text: "Controlar o layout e a aparência do site", id: "b" },
+      { text: "Gerenciar as permissões de acesso dos usuários", id: "c" },
+      { text: "Armazenar informações de configuração do tema", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é um shortcode no WordPress?",
+  options: [
+      { text: "Um código de acesso que permite inserir funcionalidades específicas em uma postagem ou página", id: "a" },
+      { text: "Um tipo de postagem personalizada", id: "b" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "c" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é um widget no WordPress?",
+  options: [
+      { text: "Um modelo de página estática", id: "a" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "b" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "c" },
+      { text: "Um componente de interface que pode ser adicionado a áreas específicas do site", id: "d" }
+  ],
+  correct: "d"
+},
+{
+  question: "Qual é a função do arquivo functions.php em um tema do WordPress?",
+  options: [
+      { text: "Gerenciar a instalação do WordPress", id: "a" },
+      { text: "Controlar as configurações de cache do navegador", id: "b" },
+      { text: "Definir funções personalizadas e modificar o comportamento do tema", id: "c" },
+      { text: "Armazenar informações de configuração do tema", id: "d" }
+  ],
+  correct: "c"
+},
+{
+  question: "O que é o WordPress Multisite?",
+  options: [
+      { text: "Um plugin para criar sites de comércio eletrônico", id: "a" },
+      { text: "Uma funcionalidade que permite gerenciar vários sites WordPress a partir de uma única instalação", id: "b" },
+      { text: "Um tema otimizado para sites de notícias e revistas", id: "c" },
+      { text: "Uma ferramenta de análise de tráfego do site", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "Como você pode atualizar o WordPress para a versão mais recente?",
+  options: [
+      { text: "Por meio do painel de administração, clicando em 'Atualizar agora'", id: "a" },
+      { text: "Editando manualmente os arquivos do núcleo do WordPress", id: "b" },
+      { text: "Desinstalando e reinstalando o WordPress", id: "c" },
+      { text: "Não é possível atualizar o WordPress", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "O que é um tipo de postagem personalizada no WordPress?",
+  options: [
+      { text: "Uma postagem que só pode ser visualizada por administradores", id: "a" },
+      { text: "Um formato especial de postagem para eventos futuros", id: "b" },
+      { text: "Um tipo de conteúdo personalizado com campos e categorias específicas", id: "c" },
+      { text: "Um modelo de página estática", id: "d" }
+  ],
+  correct: "c"
+},
+{
+  question: "O que é o WordPress Gutenberg?",
+  options: [
+      { text: "Um editor de texto básico para postagens e páginas", id: "a" },
+      { text: "Uma nova versão do WordPress otimizada para Gutenberg, um famoso escritor alemão", id: "b" },
+      { text: "Um editor de bloco de conteúdo visual usado no WordPress 5.0 e posterior", id: "c" },
+      { text: "Uma plataforma de aprendizado online para desenvolvedores WordPress", id: "d" }
+  ],
+  correct: "c"
+},
+{
+  question: "O que é um menu de navegação no WordPress?",
+  options: [
+      { text: "Uma lista de páginas do site organizadas hierarquicamente", id: "a" },
+      { text: "Um plugin para criar menus personalizados", id: "b" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "c" },
+      { text: "Um componente de interface usado para navegar entre diferentes seções do site", id: "d" }
+  ],
+  correct: "a"
+},
+{
+  question: "Como você pode adicionar uma imagem a uma postagem ou página no WordPress?",
+  options: [
+      { text: "Editando o código HTML diretamente", id: "a" },
+      { text: "Usando o editor de mídia para fazer upload e inserir a imagem", id: "b" },
+      { text: "Anexando a imagem a um e-mail e enviando para o endereço de e-mail do site", id: "c" },
+      { text: "Copiando e colando a imagem diretamente na postagem ou página", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "O que é uma tag no WordPress?",
+  options: [
+      { text: "Uma categoria hierárquica usada para organizar postagens", id: "a" },
+      { text: "Uma palavra-chave atribuída a postagens para categorizá-las e relacioná-las a outros conteúdos", id: "b" },
+      { text: "Uma marca d'água aplicada a imagens para proteção contra cópia", id: "c" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "O que é uma página estática no WordPress?",
+  options: [
+      { text: "Uma página que não pode ser modificada pelo usuário", id: "a" },
+      { text: "Uma página que contém informações estáticas e não muda frequentemente", id: "b" },
+      { text: "Um modelo de página usado como base para outras páginas", id: "c" },
+      { text: "Uma página que usa HTML estático em vez de PHP dinâmico", id: "d" }
+  ],
+  correct: "b"
+},
+{
+  question: "O que é a taxonomia no WordPress?",
+  options: [
+      { text: "Um tipo de postagem personalizada", id: "a" },
+      { text: "Um conjunto de arquivos que controlam a aparência do site", id: "b" },
+      { text: "Uma extensão para adicionar funcionalidades extras ao WordPress", id: "c" },
+      { text: "Um sistema para classificar e organizar conteúdo, como categorias e tags", id: "d" }
+  ],
+  correct: "d"
+}
+,
+{
+  question: "O que é o WordPress.com?",
+  options: [
+      { text: "Uma ferramenta de análise de tráfego do site", id: "a" },
+      { text: "Uma versão do WordPress hospedada pela Automattic", id: "b" },
+      { text: "Um editor de texto básico para postagens e páginas", id: "c" },
+      { text: "Uma plataforma de aprendizado online para desenvolvedores WordPress", id: "d" }
+  ],
+  correct: "b"
+}
     //   {
     //     question: "",
     //     image:"",
