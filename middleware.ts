fe288d4b7e97f41c05972ac88dde3c840b48420e
vituhonorato <vituhonorato.89@gmail.com>
2024-03-19 11:29:43 -0300

    add cookie options

--- middleware.ts
+++ middleware.ts
@@ -6,3 +6,21 @@ export function middleware(req:NextRequest) {
     console.log("Request", req.nextUrl.pathname)
     return NextResponse.next()
 }
+
+
+//middlewarwe only runs in the package starts in '/'
+
+// export function middleware(req:NextRequest) {
+//     if(req.nextUrl.pathname.startsWith('/')){
+//       console.log("Request", req.nextUrl.pathname)
+//     return NextResponse.next()  
+//     }
+    
+// }
+
+
+//middlewarwe only runs in the home page
+
+export const config = {
+    matcher: '/'
+}
