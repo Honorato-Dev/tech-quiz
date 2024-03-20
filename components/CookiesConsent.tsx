d91cffe6744fefff402039af3a7271db392c0cf2
vituhonorato <vituhonorato.89@gmail.com>
2024-03-20 15:20:22 -0300

    add cookie next and create cookie consent component

---
+++ components/CookiesConsent.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import {hasCookie, setCookis} from "cookies-next"
+const CookiesConsent = () => {
+
+    const [showConsent, setShowConsent]= useState()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CookiesConsent
