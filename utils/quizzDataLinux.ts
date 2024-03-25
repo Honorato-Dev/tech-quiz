0d5ec271de4c967e897072ba5261b128fd30d1d1
vituhonorato <vituhonorato.89@gmail.com>
2024-03-25 17:40:19 -0300

    add image elemente in quizzLinuxData.ts

--- utils/quizzDataLinux.ts
+++ utils/quizzDataLinux.ts
@@ -1,6 +1,7 @@
 const quizDataLinux = [
   {
     question: "Qual é o núcleo (kernel) do sistema operacional Linux?",
+    image:"",
     options: [
         { text: "Windows NT", id: "a" },
         { text: "GNU", id: "b" },
@@ -11,6 +12,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o sistema de gerenciamento de pacotes padrão utilizado em distribuições Linux como Ubuntu e Debian?",
+  image:"",
   options: [
       { text: "APT (Advanced Package Tool)", id: "a" },
       { text: "YUM (Yellowdog Updater, Modified)", id: "b" },
@@ -21,6 +23,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para listar o conteúdo de um diretório no Linux?",
+  image:"",
   options: [
       { text: "dir", id: "a" },
       { text: "ls", id: "b" },
@@ -31,6 +34,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para criar um novo diretório no Linux?",
+  image:"",
   options: [
       { text: "mkdir", id: "a" },
       { text: "newdir", id: "b" },
@@ -41,6 +45,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para remover arquivos no Linux?",
+  image:"",
   options: [
       { text: "rm", id: "a" },
       { text: "remove", id: "b" },
@@ -52,6 +57,7 @@ const quizDataLinux = [
 
 {
   question: "Qual é o diretório raiz do sistema de arquivos no Linux?",
+  image:"",
   options: [
       { text: "/", id: "a" },
       { text: "/root", id: "b" },
@@ -63,6 +69,7 @@ const quizDataLinux = [
 
 {
   question: "Qual é o comando usado para exibir o manual de um comando no Linux?",
+  image:"",
   options: [
       { text: "man", id: "a" },
       { text: "help", id: "b" },
@@ -73,6 +80,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para exibir o endereço IP de um sistema no Linux?",
+  image:"",
   options: [
       { text: "ipconfig", id: "a" },
       { text: "ifconfig", id: "b" },
@@ -83,6 +91,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para alterar as permissões de um arquivo no Linux?",
+  image:"",
   options: [
       { text: "chmod", id: "a" },
       { text: "chown", id: "b" },
@@ -93,6 +102,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para comprimir arquivos no Linux?",
+  image:"",
   options: [
       { text: "zip", id: "a" },
       { text: "compress", id: "b" },
@@ -103,6 +113,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para descomprimir arquivos no Linux?",
+  image:"",
   options: [
       { text: "unzip", id: "a" },
       { text: "uncompress", id: "b" },
@@ -113,6 +124,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o editor de texto de linha de comando mais comum no Linux?",
+  image:"",
   options: [
       { text: "nano", id: "a" },
       { text: "vim", id: "b" },
@@ -123,6 +135,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para encontrar arquivos no Linux?",
+  image:"",
   options: [
       { text: "search", id: "a" },
       { text: "find", id: "b" },
@@ -133,6 +146,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para mostrar o espaço em disco utilizado e disponível no Linux?",
+  image:"",
   options: [
       { text: "df", id: "a" },
       { text: "disk", id: "b" },
@@ -143,6 +157,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o daemon responsável pelo agendamento de tarefas no Linux?",
+  image:"",
   options: [
       { text: "cron", id: "a" },
       { text: "taskd", id: "b" },
@@ -153,6 +168,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para reiniciar o sistema no Linux?",
+  image:"",
   options: [
       { text: "restart", id: "a" },
       { text: "reboot", id: "b" },
@@ -163,6 +179,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para desligar o sistema no Linux?",
+  image:"",
   options: [
       { text: "shutdown", id: "a" },
       { text: "poweroff", id: "b" },
@@ -173,6 +190,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para ver as últimas linhas de um arquivo no Linux?",
+  image:"",
   options: [
       { text: "tail", id: "a" },
       { text: "last", id: "b" },
@@ -183,6 +201,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para ver as primeiras linhas de um arquivo no Linux?",
+  image:"",
   options: [
       { text: "head", id: "a" },
       { text: "first", id: "b" },
@@ -193,6 +212,7 @@ const quizDataLinux = [
 },
 {
   question: "Qual é o comando usado para mudar de diretório no Linux?",
+  image:"",
   options: [
       { text: "cd", id: "a" },
       { text: "dir", id: "b" },
