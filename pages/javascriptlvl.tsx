274da06da3512ec090441e6169dd751ea96fbc2b
vituhonorato <vituhonorato.89@gmail.com>
2024-04-01 14:47:13 -0300

    add rounded in divs

--- pages/javascriptlvl.tsx
+++ pages/javascriptlvl.tsx
@@ -10,13 +10,13 @@ const JavascriptLvl = () => {
         <div className="flex flex-row justify-center">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
             <Link href="/login?redirect=/javascript">
-              <div className="flex flex-col bg-white bg-opacity-50 w-44 h-44  text-center hover:scale-125 transition duration-500">
+              <div className="flex flex-col bg-white rounded-sm bg-opacity-50 w-44 h-44  text-center hover:scale-125 transition duration-500">
               <SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="font-semibold text-sm lg:text-lg">Normal</p>
               </div>
             </Link>
-            <Link href="/login?redirect=/javascript">
-              <div className="flex flex-col  bg-white bg-opacity-50 w-44 h-44 text-center hover:scale-125 transition duration-500">
+            <Link href="/login?redirect=/javascriptadv">
+              <div className="flex flex-col  bg-white rounded-sm  bg-opacity-50 w-44 h-44 text-center hover:scale-125 transition duration-500">
               <SiJavascript className=' w-10 h-10 md:w-16  md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="font-semibold text-sm lg:text-lg">Avançado</p>
               </div>
