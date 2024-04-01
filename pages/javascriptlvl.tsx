f399c1d6072233dce7402eb51e222ff09d2502e8
vituhonorato <vituhonorato.89@gmail.com>
2024-04-01 14:25:45 -0300

    add javascriptlvl 2

--- pages/javascriptlvl.tsx
+++ pages/javascriptlvl.tsx
@@ -8,16 +8,16 @@ const JavascriptLvl = () => {
     <Layout title="Javascript lvl">
       <div className="flex h-screen flex-col justify-center">
         <div className="flex flex-row justify-center">
-          <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-2">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2">
             <Link href="/login?redirect=/javascript">
-              <div className="flex flex-col text-center hover:scale-125 transition duration-500">
+              <div className="flex flex-col bg-white bg-opacity-50 w-44 h-44  text-center hover:scale-125 transition duration-500">
               <SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="font-semibold text-sm lg:text-lg">Normal</p>
               </div>
             </Link>
             <Link href="/login?redirect=/javascript">
-              <div className="flex flex-col   text-center hover:scale-125 transition duration-500">
-              <SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
+              <div className="flex flex-col  bg-white bg-opacity-50 w-44 h-44 text-center hover:scale-125 transition duration-500">
+              <SiJavascript className=' w-10 h-10 md:w-16  md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="font-semibold text-sm lg:text-lg">Avançado</p>
               </div>
             </Link>
