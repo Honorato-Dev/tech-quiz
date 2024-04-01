07c0b60989a77e72ce59d95ef10b9add4d00551d
vituhonorato <vituhonorato.89@gmail.com>
2024-04-01 18:25:40 -0300

    fix route in angular lvl

--- pages/javalvl.tsx
+++ pages/javalvl.tsx
@@ -9,13 +9,13 @@ const JavaLvl = () => {
        <div className="flex h-screen flex-col justify-center">
         <div className="flex flex-row justify-center">
           <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2">
-            <Link href="/login?redirect=/javascript">
+            <Link href="/login?redirect=/java">
               <div className="flex flex-col bg-white rounded-sm bg-opacity-50 w-96 h-96  text-center hover:scale-125 transition duration-500">
               <FaJava className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Normal</p>
               </div>
             </Link>
-            <Link href="/login?redirect=/javascriptadv">
+            <Link href="/login?redirect=/javaadv">
               <div className="flex flex-col   bg-white rounded-sm  bg-opacity-50 w-96 h-96 text-center hover:scale-125 transition duration-500">
               <FaJava className=' w-10 h-10 md:w-16   md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Avançado</p>
