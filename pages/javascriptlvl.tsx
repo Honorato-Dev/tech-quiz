7aa9318f1db9d1f0c85e9cf9ab5e9286497f4c45
vituhonorato <vituhonorato.89@gmail.com>
2024-04-01 14:17:56 -0300

    add javascriptlvl

--- pages/javascriptlvl.tsx
+++ pages/javascriptlvl.tsx
@@ -1,12 +1,31 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import Layout from '@/components/Layout';
+import Link from 'next/link';
+import React from 'react';
+import { SiJavascript } from "react-icons/si";
 
 const JavascriptLvl = () => {
   return (
-    <Layout title='Javascript lvl'>
-      
+    <Layout title="Javascript lvl">
+      <div className="flex h-screen flex-col justify-center">
+        <div className="flex flex-row justify-center">
+          <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-2">
+            <Link href="/login?redirect=/javascript">
+              <div className="flex flex-col text-center hover:scale-125 transition duration-500">
+              <SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
+                <p className="font-semibold text-sm lg:text-lg">Normal</p>
+              </div>
+            </Link>
+            <Link href="/login?redirect=/javascript">
+              <div className="flex flex-col   text-center hover:scale-125 transition duration-500">
+              <SiJavascript className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
+                <p className="font-semibold text-sm lg:text-lg">Avançado</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default JavascriptLvl
+export default JavascriptLvl;
