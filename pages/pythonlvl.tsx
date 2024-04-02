e50c67705594276204f22454f418ac882792fd21
vituhonorato <vituhonorato.89@gmail.com>
2024-04-02 10:25:47 -0300

    add pythonlvl

--- pages/pythonlvl.tsx
+++ pages/pythonlvl.tsx
@@ -1,9 +1,9 @@
 import Layout from '@/components/Layout'
 import Link from 'next/link';
 import React from 'react'
-import { SiPython } from "react-icons/si";
+import { SiPhp } from "react-icons/si";
 
-const PythonLvl = () => {
+const PhpLvl = () => {
   return (
     <Layout title='Java lvl'>
        <div className="flex h-screen flex-col justify-center">
@@ -11,13 +11,13 @@ const PythonLvl = () => {
           <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2">
             <Link href="/login?redirect=/dotnet">
               <div className="flex flex-col bg-white rounded-sm bg-opacity-50 w-96 h-96  text-center hover:scale-125 transition duration-500">
-              <SiPython  className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
+              <SiPhp  className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Normal</p>
               </div>
             </Link>
             <Link href="/login?redirect=/dotnetadv">
               <div className="flex flex-col   bg-white rounded-sm  bg-opacity-50 w-96 h-96 text-center hover:scale-125 transition duration-500">
-              <SiPython  className=' w-10 h-10 md:w-16   md:h-16 lg:w-24 lg:h-24 m-5' />
+              <SiPhp  className=' w-10 h-10 md:w-16   md:h-16 lg:w-24 lg:h-24 m-5' />
                 <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Avançado</p>
               </div>
             </Link>
@@ -29,4 +29,4 @@ const PythonLvl = () => {
   )
 }
 
-export default PythonLvl
+export default PhpLvl
