766ce54021b3ddaa449a5169aff41e85fa15167f
vituhonorato <vituhonorato.89@gmail.com>
2024-04-02 09:22:17 -0300

    dotnet  lvl

---
+++ pages/djangolvl.tsx
@@ -0,0 +1,32 @@
+import Layout from '@/components/Layout'
+import Link from 'next/link';
+import React from 'react'
+import { SiDjango } from "react-icons/si";
+
+const DjangoLvl = () => {
+  return (
+    <Layout title='Java lvl'>
+       <div className="flex h-screen flex-col justify-center">
+        <div className="flex flex-row justify-center">
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2">
+            <Link href="/login?redirect=/django">
+              <div className="flex flex-col bg-white rounded-sm bg-opacity-50 w-96 h-96  text-center hover:scale-125 transition duration-500">
+              <SiDjango  className='w-10 h-10 md:w-16 md:h-16 lg:w-24 lg:h-24 m-5' />
+                <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Normal</p>
+              </div>
+            </Link>
+            <Link href="/login?redirect=/djangoadv">
+              <div className="flex flex-col   bg-white rounded-sm  bg-opacity-50 w-96 h-96 text-center hover:scale-125 transition duration-500">
+              <SiDjango  className=' w-10 h-10 md:w-16   md:h-16 lg:w-24 lg:h-24 m-5' />
+                <p className="flex justify-center mt-20  font-semibold text-2xl lg:text-3xl lg:mt-10">Avançado</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      
+    </Layout>
+  )
+}
+
+export default DjangoLvl
